648c310a4274213e737e1af21a11e1767348d144
Pok Hengly <henglypok21012021@gmail.com>
2024-01-14 20:30:21 +0700

    add some code

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import {Header} from "@/components/Header";
 
 export default function Home() {
     return (
-        <div className="text-green-500">
-            Hello!!
+        <div
+            className="
+                bg-neutral-900
+                rounded-lg
+                h-full
+                w-full
+                overflow-hidden
+                overflow-y-auto
+          "
+        >
+            <Header>
+                Header
+            </Header>
         </div>
     )
 }
